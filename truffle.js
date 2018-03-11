954ca32c20f6dd60c4e7f6b42b2047cc7e5850b3
christinto <chrisperrymedia@gmail.com>
2018-03-12 10:14:36 +1100

    renamed truffle-config.js

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
     networks: {
         development: {
             host: "localhost",
-            port: 9545,
+            port: 8545, // renamed from 9545 to 8545 for ganache-cli
             network_id: "*" // Match any network id
         }
     }
